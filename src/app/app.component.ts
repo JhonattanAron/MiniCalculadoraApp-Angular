2501611d7de98f6bd5e4f0db552408da8dbacc7d
Aron <aron.cachago@gmail.com>
2023-08-08 20:06:14 -0500

    funcionalidad

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Mini Calculadora';
-  
+
   protected numero1:number = 0;
   protected numero2:number = 0;
   protected resultado:number = 0;
+
+  protected sumar():void{
+    this.resultado =  this.numero1 + this.numero2;
+  }
+  protected restar():void{
+    this.resultado =  this.numero1 - this.numero2;
+  }
 }
